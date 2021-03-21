a767468622ad56019a9f1c62663ce8b4d7348000
Brenda Michelle <52740973+BrendaMichellle@users.noreply.github.com>
2021-03-21 12:46:49 +0100

    router change

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {HashRouter as Router, Switch, Route} from 'react-router-dom';
 import Tours from './components/Tours';
 import Home from './components/Home';
 import Accomodations from './components/Accomodations';
@@ -8,7 +8,7 @@ import './App.css';
 function App() {
 
   return (
-    <Router>
+    <Router basename="/">
     <div className="App">
       <Switch>
         <Route path='/' exact component={Home}></Route>
