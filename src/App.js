ef28f3c6099a93fae863c781ceb84471e0bf503a
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2021-03-21 10:58:46 +0000

    used hashRouter as BrowserRouter gives a 404 page on Github Live url. On the pull request I will explain it mor

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import React from 'react';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import Tours from './components/Tours';
-import Home from './components/Home';
-import Accomodations from './components/Accomodations';
-import './App.css';
+import React from "react";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import Tours from "./components/Tours";
+import Home from "./components/Home";
+import Accomodations from "./components/Accomodations";
+import "./App.css";
 
 function App() {
-
   return (
-    <Router>
-    <div className="App">
-      <Switch>
-        <Route path='/' exact component={Home}></Route>
-        <Route path='/tours' component={Tours}/>
-        <Route path='/accomodations' component={Accomodations}/>
-      </Switch>
-    </div>
+    <Router basename="/">
+      <div className="App">
+        <Switch>
+          <Route path="/" exact component={Home}></Route>
+          <Route path="/tours" component={Tours} />
+          <Route path="/accomodations" component={Accomodations} />
+        </Switch>
+      </div>
     </Router>
   );
 }
